939cc87e037b35245219aa909d5bbd1a1fd7d285
saudhasan762 <azanali152@gmail.com>
2021-02-19 19:50:06 +0530

    Validating email of user

---
+++ ValidateEmail.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ValidateEmail {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	      System.out.println("Enter your Email: ");
+	      String email = sc.next();
+	//    String regex1 = "^[a-z]+";                             //UC1
+	//    String regex2 = "^[a-z]+@[a-z]+$";                     //UC2
+    //    String regex3 = "^[a-z]+@[a-z]+.[a-z]+$";              //UC3     
+	//    String regex5 = "^[a-z_+-.]+@[a-z]+.[a-z]+$";          //UC4
+	      String regex = "^[a-zA-Z0-9_+-.]+@[a-z]+.[a-z]+(.?)([a-z]+?)$";     //UC5
+	      boolean result = email.matches(regex);
+	      if(result) {
+	         System.out.println("Given email-id is valid");
+	      } else {
+	         System.out.println("Given email-id is not valid");
+	      }
+	}
+}
